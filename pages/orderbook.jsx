cafcf00b0a514101c2bfba306cffbf54b436cc9d
Kaustubh251002 <kmishra2510@gmail.com>
2023-03-06 22:05:40 +0530

    Order signing debug

--- pages/orderbook.jsx
+++ pages/orderbook.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -6,18 +5,59 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { NftSwapV4 } from "@traderxyz/nft-swap-sdk";
+import { ethers } from "ethers";
+import { useConnect, useAccount, useDisconnect, chain } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import React from "react";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 
 export default function Orderbook() {
     const [orders, setOrders] = useState([])
     const [filterOrder, setFilterOrder] = useState(orders)
-    const [filterByChainId, setFilterByChainId]= useState();
+    const [filterByChainId, setFilterByChainId] = useState();
     const [nftType, setNftType] = useState("Select NFT Type");
     const [show, setShow] = useState(false);
     const [selectedNft, setSelectedNft] = useState({
         tokenId: '',
-        addr: ''
+        addr: '',
+        or: ""
     })
     const handleClose = () => setShow(false);
+
+    const { connect, error, isConnecting, pendingConnector } = useConnect({
+        chainId: 5,
+        connector: new MetaMaskConnector(),
+    });
+
+    
+    const { data: account } = useAccount();
+    const { disconnect } = useDisconnect();
+    const [isConnected, setIsConnected] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
+    const [assets, setAssets] = useState([]);
+
+
+    async function handleBuy() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        try {
+            const swapSdk = new NftSwapV4(provider, signer, 5);
+            const tx = await swapSdk.fillSignedOrder(selectedNft.order);
+            const txReceipt = await fillTx.wait();
+            console.log('Filled order! 🎉', txReceipt.transactionHash);
+        }
+        catch (err) {
+            console.log(err.message);
+            setErrorMessage(true);
+            setInterval(() => {
+                router.reload(window.location.pathname);
+            }, 30000);
+        }
+    }
 
     const getOrderData = async () => {
 
@@ -36,20 +76,21 @@ export default function Orderbook() {
     };
     function handleFilter(nftType) {
         // console.log(nftType);
-        
+
         setNftType(nftType)
-        const filterByNftType = orders.filter(index => 
-            index.nftType === `${nftType}` && index.chainId==='5'
-            )
+        const filterByNftType = orders.filter(index =>
+            index.nftType === `${nftType}` && index.chainId === "5"
+        )
         // console.log(filterByNftType);
         setFilterOrder(filterByNftType)
         // console.log(filterOrder)
     }
-    const handleShow = (id, ad) => {
+    const handleShow = (id, ad, or) => {
         setShow(true)
         setSelectedNft({
             tokenId: id,
-            addr: ad
+            addr: ad,
+            order: or
         })
         // console.log(address, selectedNft)
 
@@ -89,7 +130,7 @@ export default function Orderbook() {
                     {filterOrder.filter(index => index.sellOrBuyNft === "sell").map(nft => (
                         <Card
                             style={{ width: '20rem', margin: '10px' }}
-                            onClick={() => handleShow(nft.nftTokenId, nft.nftToken)}
+                            onClick={() => handleShow(nft.nftTokenId, nft.nftToken, nft.order)}
 
                         >
                             <Card.Body>
@@ -99,7 +140,6 @@ export default function Orderbook() {
 
                                     <li>erc20Token: {nft.erc20Token}</li>
                                     <li>erc20TokenAmount: {nft.erc20TokenAmount}</li>
-
                                     <li>nftToken: {nft.nftToken}</li>
                                     <li>nftTokenId: {nft.nftTokenId}</li>
                                     <li>chainId: {nft.chainId}</li>
@@ -128,9 +168,7 @@ export default function Orderbook() {
                         Close
                     </Button>
 
-                    <Button variant="primary"
-                    
-                    >
+                    <Button variant="primary" onClick={handleBuy}>
                         Confirm Order
                     </Button>
 
