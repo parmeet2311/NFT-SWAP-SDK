f9e44ddb44a954d60262d31a120538ed205234c3
Parmeet <parmeet23112000@gmail.com>
2023-03-14 04:19:34 +0530

    customising orderbook

--- pages/orderbook.jsx
+++ pages/orderbook.jsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 
 export default function Orderbook() {
+    const router = useRouter();
     const [orders, setOrders] = useState([])
     const [filterOrder, setFilterOrder] = useState(orders)
     const [filterByChainId, setFilterByChainId] = useState();
@@ -22,7 +23,7 @@ export default function Orderbook() {
     const [selectedNft, setSelectedNft] = useState({
         tokenId: '',
         addr: '',
-        or: ""
+        order: ""
     })
     const handleClose = () => setShow(false);
 
